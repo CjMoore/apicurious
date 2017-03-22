86518bee0c07fbca108895f6c5e4d1b4ec3174d9
Charlotte Moore <mooc123@gmail.com>
2017-03-22 17:00:10 -0600

    adds styling and comments link

---
+++ spec/features/post_comments_path_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe "when user is on subreddit show and they click on comments" do
+  it "they are directed to a post show with all comments for that post" do
+
+    
+  end
+end
