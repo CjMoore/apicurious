bfa5bc1b76944b5af454a5b099923c7a032fb95c
Charlotte Moore <mooc123@gmail.com>
2017-03-22 12:41:33 -0600

    adds id to post attributes

--- app/models/hot_post.rb
+++ app/models/hot_post.rb
@@ -2,19 +2,21 @@ class HotPost
 
   attr_reader :title,
               :url,
-              :ups
+              :ups,
+              :id
 
-  def initialize(title, url, ups)
+  def initialize(title, url, ups, id)
     @title = title
     @url = url
     @ups = ups
+    @id = id
   end
 
   def self.create(name, token)
     serv = RedditService.new(token)
     serv.sub_hot_posts(name).map do |post|
       data = post[:data]
-      HotPost.new(data[:title], data[:url], data[:ups])
+      HotPost.new(data[:title], data[:url], data[:ups], data[:id])
     end
   end
 end
